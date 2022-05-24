f3162175fd6caa965530522b7a9597bd37aa5c93
akmatoff <akmatoff.exe@gmail.com>
2022-05-24 18:22:24 +0600

    UPDATE: Handle auth errors, signup and login users

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
@@ -14,49 +14,51 @@ export class AuthService {
 
     async login(username: string, password: string) {
         const user = await this.userService.getUser(username);
-        bcrypt.compare(password, user.password).then((err, same) => {
-            if (same) {
-                const token = this.jwtService.sign({ id: user.id, username });
+        let token: string;
 
-                return {
-                    message: 'Authorization successful!',
-                    token: token,
-                    user: {
-                        username: user.username,
-                        email: user.email,
-                    },
-                };
+        await bcrypt.compare(password, user.password).then(async (result) => {
+            if (result) {
+                token = await this.jwtService.signAsync({ id: user.id, username });
+            } else {
+                throw new ForbiddenException('Password or user did not match')
             }
         });
+
+        return {
+            message: 'Authorization successful!',
+            token: token,
+            user: {
+                username: user.username,
+                email: user.email,
+            },
+        };
     }
 
     async signup(user: User) {
-        try {
-            await bcrypt.hash(user.password, 10, (err, hash) => {
-                if (err) throw Error('An error occured while creating');
+        if (!user.password) throw new BadRequestException('No password provided')
 
-                this.userService.createUser({
-                    ...user,
-                    password: hash,
-                    isAdmin: false
-                });
+        const hash = await bcrypt.hash(user.password, 12)
 
-                return {
-                    message: 'User created successfully!',
-                    user: {
-                        id: user.id,
-                        username: user.username,
-                        email: user.email,
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                    },
-                };
-            });
-        } catch (error) {
-            return {
-                message: error.message,
-            };
-        }
+        if (!hash) throw new BadRequestException('An error occured while creating a user')
+
+        await this.userService.createUser({
+            ...user,
+            password: hash,
+            isAdmin: false,
+        }).catch((err) => {
+            throw new BadRequestException(err.message);
+        })
+
+        return {
+            message: 'User created successfully!',
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            },
+        };
     }
 
     logout() {}
