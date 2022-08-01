cc1ed237bcaff63a92ea670490c122b1decd40ae
akmatoff <akmatoff.exe@gmail.com>
2022-08-01 17:29:52 +0600

    UPDATE: project structure refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,19 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
-import { PrismaService } from './services/prisma.service';
-import { QuizService } from './services/quiz/quiz.service';
-import { QuizController } from './controllers/quiz/quiz.controller';
-import { UserModule } from './controllers/user/user.module';
-import { ParagraphService } from './services/paragraph/paragraph.service';
-import { PersonService } from './services/person/person.service';
+import { PrismaService } from './prisma.service';
+import { QuizService } from './quiz/quiz.service';
+import { QuizController } from './quiz/quiz.controller';
+import { ParagraphService } from './paragraph/paragraph.service';
+import { PersonService } from './person/person.service';
 import { join } from 'path';
-import { ParagraphController } from './controllers/paragraph/paragraph.controller';
+import { ParagraphController } from './paragraph/paragraph.controller';
 import { MorganModule } from 'nest-morgan';
-import { PersonController } from './controllers/person/person.controller';
-import { AuthController } from './controllers/auth/auth.controller';
-import { AuthService } from './services/auth/auth.service';
+import { PersonController } from './person/person.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { UserService } from './services/user/user.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
@@ -29,7 +27,6 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
             rootPath: join(__dirname, '..', 'public'),
         }),
         MorganModule,
-        UserModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: {
@@ -38,6 +35,6 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
         })
     ],
     controllers: [AppController, QuizController, ParagraphController, PersonController, AuthController],
-    providers: [QuizService, PrismaService, ParagraphService, PersonService, UserService, AuthService, JwtStrategy, JwtAuthGuard],
+    providers: [QuizService, PrismaService, ParagraphService, PersonService, AuthService, JwtStrategy, JwtAuthGuard],
 })
 export class AppModule {}
--- src/controllers/user/user.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('api/user')
-export class UserController {}
