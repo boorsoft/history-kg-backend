8a828f886897c3d6c8f9012d647d287b2827aa6e
akmatoff <akmatoff.exe@gmail.com>
2022-08-01 17:31:30 +0600

    Revert "UPDATE: project structure refactor"
    This reverts commit cc1ed237bcaff63a92ea670490c122b1decd40ae.

---
+++ src/services/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/types/types';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class UserService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async getUser(username: string) {
+        return this.prismaService.user.findFirst({
+            where: {
+                username,
+            },
+        });
+    }
+
+    async createUser(user: User) {
+        return this.prismaService.user.create({
+            data: {
+                ...user
+            }
+        })
+    }
+}
